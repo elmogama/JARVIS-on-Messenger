0b1e5e4ee1114563249eebcdfa10072e1d7b0fbc
Mohamed Elmogaber <melmogaber@gmail.com>
2024-04-23 23:04:28 -0400

    Tests

---
+++ modules/tests/test_aq.py
@@ -0,0 +1,9 @@
+﻿import modules
+
+
+def test_aq():
+    assert ('aq' == modules.process_query('Give me the aq of London,GB')[0])
+    assert ('aq' == modules.process_query('aq of Paris,FR')[0])
+    assert ('aq' == modules.process_query('What\'s the aq of US')[0])
+    assert ('aq' != modules.process_query('How was your day in london')[0])
+    assert ('aq' != modules.process_query('Tell me about the aquarium in CH!')[0])
